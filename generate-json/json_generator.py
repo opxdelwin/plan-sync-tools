0b988f5e4ad18651e36a6cd8ca0407d39640cd59
Delwin Mathew <84124091+opxdelwin@users.noreply.github.com>
2024-12-05 13:53:50 +0530

    make timeslots dynamic

--- generate-json/json_generator.py
+++ generate-json/json_generator.py
@@ -13,6 +13,10 @@ import csv
 import json
 import os
 
+def get_time_slots(reader: csv.DictReader) -> list[str]:
+    header = reader.fieldnames
+    return [slot for slot in header if slot not in ["DAY", "Section"]]
+
 def getDayInSchema(abbr:str):
     if(abbr.upper() == 'MON'): return 'monday'
     if(abbr.upper() == 'TUE'): return 'tuesday'
@@ -24,7 +28,7 @@ def getDayInSchema(abbr:str):
     
 
 academic_year = "2024 - 2025"
-sem = "SEM3 - SCE"
+sem = "SEM4 - SCE"
 output_dir = 'output/' + academic_year + '/' + sem + '/'
 
 modified_count = 0
@@ -37,8 +41,8 @@ output_dict = {
     "meta": {
         "section": "a10",
         "type": "norm-class",
-        "revision": "Revision 3.0",
-        "effective-date": "Sep 26, 2024",
+        "revision": "Revision 1.0",
+        "effective-date": "Dec 4, 2024",
         "contributor": "PlanSync Admin :)",
         "isTimetableUpdating": False
     },
@@ -46,88 +50,28 @@ output_dict = {
     "data": {}
 }
 
-# 2nd,3rd year
-time_slots = [
-    "08:00 - 09:00",
-    "09:00 - 10:00",
-    "10:00 - 11:00",
-    "11:00 - 12:00",
-    "12:00 - 13:00",
-    "13:00 - 14:00",
-    "14:00 - 15:00",
-    "15:00 - 16:00",
-    "16:00 - 17:00",
-    "17:00 - 18:00"
-]
-
-# 1st year Sch B
-# time_slots = [
-#     "08:00 - 10:00",
-# "10:00 - 11:00",
-# "10:20 - 12:20",
-# "10:20 - 11:20",
-# "09:50 - 10:50",
-# "11:20 - 12:20",
-# "11:00 - 12:00",
-# "12:00 - 13:00",
-# "12:20 - 13:30",
-# "13:50 - 14:50",
-# "14:00 - 16:00",
-# "14:40 - 15:40",
-# "15:00 - 16:00",
-# "16:00 - 18:00",
-# "16:00 - 17:00",
-# "16:20 - 17:20"
-# ]
-
-# # 1st Year Scheme A
-# time_slots = [
-#     "08:00 - 11:00",
-#     "08:00 - 10:00",
-#     "08:00 - 09:00",
-#     "09:00 - 10:00",
-#     "09:50 - 10:50",
-#     "09:50 - 11:50",
-#     "10:00 - 11:00",
-#     "10:20 - 11:20",
-#     "10:50 - 11:50",
-#     "10:00 - 12:00",
-#     "11:00 - 14:00",
-#     "11:10 - 12:10",
-#     "11:30 - 12:30",
-#     "11:00 - 12:00",
-#     "11:10 - 12:10",
-#     "11:20 - 12:20",
-#     "11:50 - 12:50",
-#     "12:20 - 13:20",
-#     "12:30 - 13:30",
-#     "12:20 - 14:20",
-#     "13:20 - 14:20",
-#     "15:00 - 16:00",
-#     "15:00 - 18:00",
-#     "15:30 - 17:30",
-#     "15:30 - 16:30",
-#     "16:40 - 17:40"
-# ]
+time_slots = None
 
 
-for csvFile in os.listdir('./generate-json/csv/output/'):
-    if not os.path.isfile('./generate-json/csv/output/' + csvFile):
+for csvFile in os.listdir('./generate-json/csv/input/'):
+    if not os.path.isfile('./generate-json/csv/input/' + csvFile):
         print('Not a file: ' + csvFile + '\t SKIPPING')
         continue
 
     # Open the CSV file for reading
-    with open('./generate-json/csv/output/' + csvFile, mode='r') as file:
+    with open('./generate-json/csv/input/' + csvFile, mode='r') as file:
         print('Gen -> ' + csvFile)
         # Create a CSV reader with DictReader
         csv_reader = csv.DictReader(file, delimiter=",")
+        if(time_slots == None):
+            time_slots = get_time_slots(csv_reader)
     
         # Iterate through each row in the CSV file
         for row in csv_reader:
             
             # Modify output_dict and add day-wise value
             # update keys as per csv header
-            day_in_schema = getDayInSchema(row["Day"])
+            day_in_schema = getDayInSchema(row["DAY"])
             periods = []
             
             for slot in time_slots:
